d545c9086c04ee57eea63be31c308df82360bbdb
Akash <akash@careconnect.com>
2025-11-21 14:16:03 +0530

    Fix: Use APP_DIRS for template discovery

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'core', 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
